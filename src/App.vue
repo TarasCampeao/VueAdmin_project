28cce1cebaedba2c7cf72af905cf71b4b2aad28c
Taras Vitovshchyk <vitovstreet@gmail.com>
2018-10-13 18:38:05 +0300

    added Users component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<style lang="sass">
+    @import url('https://use.fontawesome.com/releases/v5.3.1/css/all.css')
+    @import "./assets/style/main.sass"
+    @import "./assets/style/media.sass"
+</style>
 <template>
   <div id="app">
     <Users/>
@@ -5,11 +10,12 @@
 </template>
 
 <script>
+import Users from './components/Users';
 
 export default {
   name: 'app',
   components: {
-
+  	Users
   },
 };
 </script>
---
+++ src/components/Users.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="container">
+
+	</div>
+</template>
+
+<script>
+import fetchData from '../api/test.json';
+
+export default {
+	name: 'Users',
+	components: {
+
+	},
+	data() {
+		return {
+
+		}
+	}
+}	
+
+</script>
