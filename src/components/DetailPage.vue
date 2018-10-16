b769209256bc296114bdc76737860d5c3c17e5a7
Taras Vitovshchyk <vitovstreet@gmail.com>
2018-10-17 02:47:51 +0300

    added template for item page

---
+++ src/components/DetailPage.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="container">
+		<div>Name item: {{ item.name }}</div>
+		<div>Location item: {{ item.location }}</div>
+		<div>Currency item: {{ item.currency }}</div>
+		<div class="all_info">{{ item.info }}</div>
+	</div>
+</template>
+
+
+<script>
+import fetchData from '../api/test.json';	
+
+export default {
+	name: 'DetailPage',
+	data() {
+		return {
+			
+		}
+	}
+}
+
+</script>
